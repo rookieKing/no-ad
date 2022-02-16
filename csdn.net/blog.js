c32ab4c2fd7ce8e511b53366e27f47b0956c2914
rookie <rookielei@gmail.com>
2022-02-16 20:12:47 +0800

    使未登录状态可以复制代码

--- csdn.net/blog.js
+++ csdn.net/blog.js
@@ -1,1 +1,14 @@
 console.log('--- blog');
+window.addEventListener('load', () => {
+  // 未登录时 直接复制代码
+  document.querySelectorAll('.hljs-button.signin').forEach(btn => {
+    btn.setAttribute('data-title', '复制');
+    btn.addEventListener('click', () => {
+      navigator.clipboard.writeText(btn.parentElement.innerText);
+      btn.setAttribute('data-title', 'Copied!');
+    }, false);
+    btn.addEventListener('mouseleave', () => {
+      btn.setAttribute('data-title', '复制');
+    });
+  });
+});
